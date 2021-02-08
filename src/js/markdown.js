c3743fe2d1e35e8a74d4f38e832cccfbfe8ebacb
sunjing <serdeemail@gmail.com>
2021-02-08 17:20:53 +0800

    新增回到顶部按钮

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,7 +1,27 @@
-marked.setOptions({
-  langPrefix: 'hljs ',
-  highlight: function (code, language) {
-    const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
-    return hljs.highlight(validLanguage, code).value;
-  },
-});
+// highlight.js 配置
+if (window.marked) {
+  window.marked.setOptions({
+    langPrefix: 'hljs ',
+    highlight: function (code, language) {
+      const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+      return hljs.highlight(validLanguage, code).value;
+    },
+  });
+}
+
+// 回到顶部
+(function () {
+  var ele = document.createElement('div');
+  ele.id = 'J_Scroll2Top';
+  ele.className = 'scroll-2-top';
+  ele.innerHTML = '↑';
+  ele.title = '回到顶部';
+  document.body.appendChild(ele);
+
+  ele.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, false);
+})();
